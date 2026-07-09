162dcd248ac4a5d8cf5745639dc489c6ad7dccfb
sahankavishka <sahankavi100@gmail.com>
2026-07-09 17:08:17 +0000

    my first bash script

---
+++ script01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#system information
+echo "======SYSTEM INFORMATION======"
+echo "User : $USER"
+echo "Hostname : $HOSTNAME"
+echo "Current Folder : $(pwd)"
+echo "Current Date : $(date)"
+ls
+echo "=============================="
