669c9ed0bf0bc3cc55ae44d423f9744b1c01fda9
sahankavishka <sahankavi100@gmail.com>
2026-07-11 08:30:40 +0000

    newly learn how to use nested if

---
+++ Script-02/script02-2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#===================
+#Login System
+#===================
+
+echo
+echo "========================"
+echo "Login Your Admin Account"
+echo "========================"
+echo
+
+#user inputs
+read -p "Enter your username :" username
+read -p "Enter your password :" password
+
+#logic with nested if
+if [ "$username" = "administrator" ]; then
+
+  if [ "$password" = "admin123" ]; then
+  echo
+  echo "====Login Successfull==="
+  else 
+   echo "Wrong Paasword"
+  fi
+
+else
+ echo "Invalid Username"
+fi
+
+echo
+echo "========================"
+echo
+
